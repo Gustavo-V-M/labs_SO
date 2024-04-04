60ff52dec153949a730509d6e591c11b9842bd4e
Gustavo V. Mitraud <gvmitraud@protonmail.com>
2024-04-04 15:27:55 -0300

    Adicionado o arquivo de testes

---
+++ projeto01/testes.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# compile thread code and execute tests
+
+g++ ./escada_rolante_threads.cpp -o threads.out
+
+FILES=$(ls ./input/)
+OUTPUT_FOLDER="./output/"
+
+for FILE in $FILES; do
+	RESULT=$(./threads.out ./input/$FILE)
+	EXPECTED=$(head -n 1 $OUTPUT_FOLDER$FILE)
+	if [[ $RESULT -ne $EXPECTED ]]; then
+		echo "($FILE) teste falhou (Esperado: $EXPECTED) (RESULTADO: $RESULT)" >>result.txt
+	fi
+done
